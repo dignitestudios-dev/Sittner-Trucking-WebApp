3c33c598c1a365faf8eb80a31fbd908325565d90
zackcole <zeeshan.ahmed@launchbox.pk>
2024-10-18 04:18:54 +0500

    update:mobile optimization

--- src/firbase/FirebaseInit.js
+++ src/firbase/FirebaseInit.js
@@ -1,14 +1,14 @@
 import { initializeApp } from "firebase/app";
 
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDbY64yhhVD7uTOpxLdP0eYZRWJfrLiVLw",
-    authDomain: "sittnertruck.firebaseapp.com",
-    projectId: "sittnertruck",
-    storageBucket: "sittnertruck.appspot.com",
-    messagingSenderId: "205400416707",
-    appId: "1:205400416707:web:8e3a7dedd043d73dd99d76",
-    measurementId: "G-REKQCKK3RJ"
-  };
+// const firebaseConfig = {
+//     apiKey: "AIzaSyDbY64yhhVD7uTOpxLdP0eYZRWJfrLiVLw",
+//     authDomain: "sittnertruck.firebaseapp.com",
+//     projectId: "sittnertruck",
+//     storageBucket: "sittnertruck.appspot.com",
+//     messagingSenderId: "205400416707",
+//     appId: "1:205400416707:web:8e3a7dedd043d73dd99d76",
+//     measurementId: "G-REKQCKK3RJ"
+//   };
   
-  const app = initializeApp(firebaseConfig);
+//   const app = initializeApp(firebaseConfig);
