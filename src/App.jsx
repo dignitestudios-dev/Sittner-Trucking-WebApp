ff10df9f7a474de549f1ceac68b0b59f39c72af8
zackcole <zeeshan.ahmed@launchbox.pk>
2024-10-18 23:18:57 +0500

    update:pwa

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,7 @@ import './App.css'
 import { MyContextProvider } from './context/GlobalContext'
 import AppRoutes from './routes/AppRoutes'
 function App() {
-  window.addEventListener('load', async () => {
-    if (navigator.serviceWorker) {
-      await navigator.serviceWorker.register('/service-worker.ts')
-      console.log('service worker registered successfully')
-    }
-  })
+ 
   return (
     <MyContextProvider>
       <AppRoutes />
