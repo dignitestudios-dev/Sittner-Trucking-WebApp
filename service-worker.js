2064e3794f8cdba76b8381a2b8d26a2248521166
zackcole <zeeshan.ahmed@launchbox.pk>
2024-11-08 13:35:11 -0700

    Notification optimize

--- service-worker.js
+++ service-worker.js
@@ -29,3 +29,14 @@
         caches.match(e.request).then((res) => (res ? res : fetch(e.request)))
     );
     });
+
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/firebase-messaging-sw.js')
+          .then(function(registration) {
+            console.log('Service Worker registered with scope: ', registration.scope);
+          })
+          .catch(function(error) {
+            console.error('Service Worker registration failed: ', error);
+          });
+      }
+      
