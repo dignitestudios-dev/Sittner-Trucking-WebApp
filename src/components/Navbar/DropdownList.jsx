ecb213a46616218a212cdb956a4b853cb6222b6b
zackcole <zeeshan.ahmed@launchbox.pk>
2024-11-11 15:04:36 -0700

    update

--- src/components/Navbar/DropdownList.jsx
+++ src/components/Navbar/DropdownList.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef, useState } from "react";
+import React, { useCallback, useContext, useEffect, useRef, useState } from "react";
 import { MyContext } from "../../context/GlobalContext";
 import {
   addDoc,
