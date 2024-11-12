aa173cf318518819cd0f17212c1710515c08f8d2
zackcole <zeeshan.ahmed@launchbox.pk>
2024-11-12 23:23:21 +0500

    update:notification updated

--- src/components/Navbar/DropdownList.jsx
+++ src/components/Navbar/DropdownList.jsx
@@ -38,7 +38,7 @@ export default function DropdownList() {
     setIsDropdown(!IsDropdownOpen);
   };
   useEffect(() => {
-    if (pushNotification > 0 && UserRole === "user") {
+    if (pushNotification > 0 || UserRole == "user") {
       toast.success(NotTitle || "New Notification", {
         position: "top-right",
         autoClose: 3000,
